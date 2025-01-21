ffbcf88eb181865bcdbca42f45f3032e95c09503
ana <ana@anniecannons.com>
2025-01-21 15:37:57 -0800

    Set up Vite React and add initial content

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to DejaVuAI!</h1>
+      <p>Start building your Vite-powered React app here.</p>
+    </div>
+  );
+}
+
+export default App;

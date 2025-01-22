949583fe7cd4ae8d68661ec35265c8fd06d57405
ana <ana@anniecannons.com>
2025-01-22 09:47:29 -0800

    added chakra

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "./components/ui/provider"; // Adjust path as needed
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+// import { Provider } from "@/components/ui/provider"
+// import React from "react"
+// import ReactDOM from "react-dom/client"
+// import App from "./App"
+
+// ReactDOM.createRoot(document.getElementById("root")!).render(
+//   <React.StrictMode>
+//     <Provider>
+//       <App />
+//     </Provider>
+//   </React.StrictMode>,
+// );
